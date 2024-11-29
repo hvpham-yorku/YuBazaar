0fdc4d8a1288d97b24d7cf9381610364bf32af80
krish <krishshah1411@gmail.com>
2024-11-28 19:04:45 -0500

    ticket #3SmtYrIY Item condition tags implemented

--- YU Bazaar/demo/src/main/java/com/example/demo/controller/ItemController.java
+++ YU Bazaar/demo/src/main/java/com/example/demo/controller/ItemController.java
@@ -29,6 +29,13 @@ public class ItemController {
                           @RequestParam String location,
                           @RequestParam String description,
                           Model model) {
+        // Validate wear options
+        List<String> validWearOptions = Arrays.asList("new", "used (like new)", "used", "poor");
+        if (!validWearOptions.contains(wear)) {
+            model.addAttribute("error", "Invalid wear condition selected.");
+            return "home_page";
+        }
+
         // Validate location
         List<String> validLocations = Arrays.asList("ACE", "ACW", "AO", "ATK", "BC", "BCSS", "BRG", "BSB", "BU",
                 "CB", "CC", "CFA", "CFT", "CLH", "CMB", "CSQ", "CUB", "DB", "ELC", "FC", "FL", "FRQ", "FTC", "GH",
@@ -38,7 +45,7 @@ public class ItemController {
 
         if (!validLocations.contains(location)) {
             model.addAttribute("error", "Invalid location selected.");
-            return "home_page"; // Or another error page
+            return "home_page";
         }
 
         Item item = new Item();
@@ -51,6 +58,7 @@ public class ItemController {
 
         return "redirect:/home";
     }
+
 
     @PostMapping("/delete-item")
     public String deleteItem(@RequestParam Long id) {
